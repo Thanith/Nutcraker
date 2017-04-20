3ccce0576e92ab49d4cca49c05c556049da5f908
Thanith <nathan.boutte@viacesi.fr>
2017-04-20 17:41:38 +0200

    [mix] updating app name

--- mix.exs
+++ mix.exs
@@ -1,8 +1,8 @@
-defmodule Mailgun.Mixfile do
+defmodule ToolBox.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :mailgun,
+    [app: :toolbox,
      version: "0.0.1",
      elixir: "~> 1.4",
      deps: deps(),
