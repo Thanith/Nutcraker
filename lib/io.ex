d83d47ade364fc1a2a6fa75d23d7eb31d0eec7cf
Thanith <nathan.boutte@viacesi.fr>
2017-04-20 17:40:25 +0200

    initial commit

---
+++ lib/io.ex
@@ -0,0 +1,8 @@
+defmodule ToolBox.IO do
+
+  def inspect(element) do
+    IO.inspect "---------- ---------- ----------"
+    IO.inspect element
+    IO.inspect "---------- ---------- ----------"
+  end
+end
